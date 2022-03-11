8402836fa8baf11823de63a63b54630405feaada
chd000 <stibbot@yandex.ru>
2022-03-11 13:40:29 +0300

    forms module imported

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +14,7 @@ import { ProductService } from './product.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule, FormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
